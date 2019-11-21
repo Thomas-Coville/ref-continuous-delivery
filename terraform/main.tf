2700ad8d985b34658e0d24954c84381be2961266
Thomas-Coville <tcoville@landr.com>
2019-11-20 21:39:18 -0800

    test google cloud

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,3 +9,17 @@ terraform {
     }
   }
 }
+
+provider "google" {
+  project     = "ref-cd"
+  region      = "us-central1"
+  zone        = "us-central1-c"
+}
+
+resource "google_pubsub_topic" "example" {
+  name = "example-topic"
+
+  labels = {
+    foo = "bar"
+  }
+}
